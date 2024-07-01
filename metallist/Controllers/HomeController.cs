504afd732b4204523531fc2abb13f4052c3ed838
QtaZva <qtazva@mail.ru>
2024-07-02 00:27:08 +0900

    Добавьте файлы проекта.

---
+++ metallist/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using metallist.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace metallist.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
